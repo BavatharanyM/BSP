704ceeaffd5d86f713720f73009b7e19a5b95f41
bavatharany <m.bavatharany@gmail.com>
2025-01-03 13:09:44 +0530

    Initial commit

---
+++ detabaseconnection/src/main/java/com/database/connection/detabaseconnection/DetabaseconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.database.connection.detabaseconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetabaseconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetabaseconnectionApplication.class, args);
+	}
+
+}
---
+++ detabaseconnection/src/test/java/com/database/connection/detabaseconnection/DetabaseconnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.connection.detabaseconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetabaseconnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
